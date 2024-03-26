b123de3aa3c99233457a97e900a9385a06d88a12
rafaelnicolau <rafael.g.nicolau@gmail.com>
2024-03-26 13:05:33 +0100

    Create main functionality and story lines

--- WildernessEscape.py
+++ WildernessEscape.py
@@ -5,9 +5,20 @@ class TreeNode:
     
     def add_child(self, node: str) -> None:
         self.choices.append(node)
+    
+    def traverse(self) -> str:
+        story_node = self
+        print(story_node.story_piece)
 
-user_choice = input("What's your name?\n")
-print("Once upon a time...")
+        while len(story_node.choices) > 0:
+            choice = input("Enter 1 or 2 to continue the story:\n")
+            if choice not in ["1", "2"]:
+                continue
+            else:
+                chosen_index = int(choice) - 1
+                chosen_child = story_node.choices[chosen_index]
+                print(chosen_child.story_piece)
+                story_node = chosen_child
 
 story_root = TreeNode(
 """
@@ -27,6 +38,22 @@ Do you:
 2 ) Yell 'Hooray!'
 """)
 
+choice_a1 = TreeNode(
+"""
+The bear returns and tells you it's been a rough week. After making peace with
+a talking bear, he shows you the way out of the forest.
+
+YOU HAVE ESCAPED THE WILDERNESS.
+""")
+
+choice_a2 = TreeNode(
+"""
+The bear returns and tells you that bullying is not okay before leaving you alone
+in the wilderness.
+
+YOU REMAIN LOST.
+""")
+
 choice_b = TreeNode(
 """
 You come across a clearing full of flowers. 
@@ -36,4 +63,31 @@ Do you:
 2 ) Explain that the bear scared you.
 """)
 
-# print(story_root.story_piece)
+choice_b1 = TreeNode(
+"""
+The bear is unamused. After smelling the flowers, it turns around and leaves you alone.
+
+YOU REMAIN LOST.
+""")
+
+choice_b2 = TreeNode(
+"""
+The bear understands and apologizes for startling you. Your new friend shows you a 
+path leading out of the forest.
+
+YOU HAVE ESCAPED THE WILDERNESS.
+""")
+
+story_root.add_child(choice_a)
+story_root.add_child(choice_b)
+choice_a.add_child(choice_a1)
+choice_a.add_child(choice_a2)
+choice_b.add_child(choice_b1)
+choice_b.add_child(choice_b2)
+
+def main() -> None:
+    print("Once upon a time...")
+    story_root.traverse()
+
+if __name__ == "__main__":
+    main()
