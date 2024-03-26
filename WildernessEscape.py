824e3f369f778bfd0760f83e15be4697182e57e2
rafaelnicolau <rafael.g.nicolau@gmail.com>
2024-03-26 12:03:58 +0100

    Add first and second choices for story root

--- WildernessEscape.py
+++ WildernessEscape.py
@@ -2,8 +2,11 @@ class TreeNode:
     def __init__(self, story_piece: str) -> None:
         self.story_piece = story_piece
         self.choices = []
+    
+    def add_child(self, node: str) -> None:
+        self.choices.append(node)
 
-
+user_choice = input("What's your name?\n")
 print("Once upon a time...")
 
 story_root = TreeNode(
@@ -15,3 +18,22 @@ Do you:
 1 ) Roar back! 
 2 ) Run to the left...
 """)
+
+choice_a = TreeNode(
+"""
+The bear is startled and runs away.
+Do you:
+1 ) Shout 'Sorry bear!'
+2 ) Yell 'Hooray!'
+""")
+
+choice_b = TreeNode(
+"""
+You come across a clearing full of flowers. 
+The bear follows you and asks 'what gives?'
+Do you:
+1 ) Gasp 'A talking bear!'
+2 ) Explain that the bear scared you.
+""")
+
+# print(story_root.story_piece)
