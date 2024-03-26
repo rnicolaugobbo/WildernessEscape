16582e2007e30134af490ba92cf1260d5617f38a
rafaelnicolau <rafael.g.nicolau@gmail.com>
2024-03-26 11:51:10 +0100

    Create story root as tree node

--- WildernessEscape.py
+++ WildernessEscape.py
@@ -2,8 +2,16 @@ class TreeNode:
     def __init__(self, story_piece: str) -> None:
         self.story_piece = story_piece
         self.choices = []
-    
-    
 
 
 print("Once upon a time...")
+
+story_root = TreeNode(
+"""
+You are in a forest clearing. 
+There is a path to the left. 
+A bear emerges from the trees and roars! 
+Do you:  
+1 ) Roar back! 
+2 ) Run to the left...
+""")
